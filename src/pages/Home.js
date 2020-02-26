8c927f80809fc9f5806e98eb0954fa07f074a8ee
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-02-26 09:27:35 -0500

    Second Push

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// API Keys
+const defaultKey = "a0114a85a2ca2ec5ce78200957401e61";
+
+function Home() {
+  return (
+      <div>
+        <h2>Home</h2>
+      </div>
+  );
+}
+
+export default Home;
